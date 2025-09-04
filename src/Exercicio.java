e25a937a39878fc07d1e570bb6bf0c97b55a877e
Kayque <kayquecarvalho020@gmail.com>
2025-09-04 19:36:39 -0300

    Refactor: Melhorando a visualização com a estrutura de classe para cada exercício

--- src/Exercicio.java
+++ src/Exercicio.java
@@ -1,3 +1,5 @@
+// Seja bem-vindo! Esse código contém a resolução da lista de 15 exercícios presentes no README
+
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
@@ -7,69 +9,106 @@ public class Exercicio {
         Scanner scanner = new Scanner(System.in);
         System.out.println("=== Programa de conversão de metros para centímetros e milímetros ===\n");
         System.out.println("Digite o número de metros para serem transformado em centímetros e milímetros");
-        double metros = 20;//scanner.nextDouble();
+        double metros = scanner.nextDouble();
         double centrimetros = metros * 100;
         double milimetros = centrimetros * 10;
 
         System.out.println(metros + " metro(s) equivalem a " + centrimetros + " centímetros e " + milimetros + " milímetros");
 
-        //2. Crie um programa que leia a idade de uma pessoa e diga se ela pode votar (idade ≥ 16).
-        int idade = 33;
-
-        if ( idade >= 16 ) {
-            System.out.println("Você já tem idade suficiente para votar");
-        } else if (idade < 16 && idade > 0) {
-            System.out.println("Você ainda não tem idade o suficiente para votar. A idade minima é de 16 anos");
-        } else {
-            System.out.println("Idade inválida");
-        }
-
-        //3. Escreva um programa que leia dois números e mostre todos os números inteiros entre eles.
-        int num1 = 10;
-        int num2 = 20;
-        System.out.println("=== Todos os números inteiros entre " + num1 + " e " + num2+ " ===");
-        while (num1 < num2 - 1) {
-            num1 ++;
-            System.out.println(num1);
-        }
-        System.out.println("===");
-
-        //Ex.4 Crie uma função que receba um número inteiro e retorne seu fatorial. Teste no main.
-
-        System.out.println("\n=== Digite um número inteiro para saber o seu fatorial ===");
-        int numFatorial = scanner.nextInt();
-
-        System.out.println("O fatorial de " + numFatorial + " equivale a " + calcularFatorial(numFatorial));
-
-        System.out.println("Escreva um número inteiro para a tabuada");
-        int numTabuada = scanner.nextInt();
-        tabuada(numTabuada);
-
-
         scanner.close();    //Fechando Scanner
     }
 
-    static int calcularFatorial (int numero ) {
-        if (numero == 0 ) {
-            return 1;
+    //Ex. 2 Crie um programa que leia a idade de uma pessoa e diga se ela pode votar (idade ≥ 16).
+    public static class exercicio2 {
+        public static void main(String[] args) {
+            System.out.println("=== Validação de idade para votar ===");
+            System.out.println("Digite a idade a ser verificada");
+            Scanner scanner = new Scanner(System.in);
+
+            int idade = scanner.nextInt();
+            if ( idade >= 16 ) {
+                System.out.println("Já tem idade suficiente para votar");
+            } else if ( idade > 0) {
+                System.out.println("Ainda não tem idade o suficiente para votar. A idade minima é de 16 anos");
+            } else {
+                System.out.println("Idade inválida");
+            }
+
+            scanner.close();
         }
-        if (numero < 0 ) {
-            return -1;
+    }
+
+    //Ex. 3 Escreva um programa que leia dois números e mostre todos os números inteiros entre eles
+    public static class exercicio3 {
+        public static void main (String[] args) {
+            System.out.println("===Programa para lista números entre dois valores===");
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Digite o primeiro número: ");
+            int num1 = scanner.nextInt();
+
+            System.out.println("Digite o segundo número: ");
+            int num2 = scanner.nextInt();
+
+           int maior = Math.max(num1, num2);
+           int menor = Math.min(num1, num2);
+
+            System.out.println("=== Todos os números inteiros entre " + menor + " e " + maior+ " ===");
+            while (menor < maior - 1) {
+                menor ++;
+                System.out.println(menor);
+            }
+            System.out.println("===");
+            scanner.close();
         }
-        int resultado = 1;
-        for ( int numfatorado = 1;  numfatorado <= numero ; numfatorado++){
-            resultado *= numfatorado;
+    }
+
+
+    //Ex.4 Crie uma função que receba um número inteiro e retorne seu fatorial. Teste no main.
+    public static class exercicio4 {
+        public static void main (String[] args) {
+            System.out.println("\n=== Digite um número inteiro para saber o seu fatorial ===");
+            Scanner scanner = new Scanner(System.in);
+            int numFatorial = scanner.nextInt();
+
+            System.out.println("O fatorial de " + numFatorial + " equivale a " + calcularFatorial(numFatorial));
+            scanner.close();
         }
-        return resultado;
+        static int calcularFatorial (int numero ) {
+            if (numero == 0 ) {
+                return 1;
+            }
+            if (numero < 0 ) {
+                return -1;
+            }
+            int resultado = 1;
+            for ( int numfatorado = 1;  numfatorado <= numero ; numfatorado++){
+                resultado *= numfatorado;
+            }
+            return resultado;
+        }
     }
 
     //Ex.06 Crie uma função que receba um número inteiro e mostre a tabuada desse número até 10.
-    static void tabuada (int numeroInt) {
-        for ( int i = 1; i <= 10; i++) {
-            int calculoTabuada = i * numeroInt;
-            System.out.println( i + " X " + numeroInt + " = " + calculoTabuada );
-            }
+
+    public static class exercicio6 {
+        public static void main(String[] args) {
+            System.out.println("=== Tabuada de um número inteiro ===");
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Digite um número inteiro para a tabuada: ");
+            int numTabuada = scanner.nextInt();
+            tabuada(numTabuada);
+
+            scanner.close();
         }
+        static void tabuada (int numeroInt) {
+            for ( int i = 1; i <= 10; i++) {
+                int calculoTabuada = i * numeroInt;
+                System.out.println( i + " X " + numeroInt + " = " + calculoTabuada );
+                }
+            }
+    }
 
     //7)	Crie uma função que receba dois números e retorne o maior deles.
     public static class Exercicio7  {
